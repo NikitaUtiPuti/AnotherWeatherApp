001587a9c3983ca8ee8495e751e90aa8a3773dfc
Nikita <ymnooi1998@yandex.ru>
2021-12-15 14:47:41 +0300

    Background and weather indicators icons added. Current data and BG fetching functional completed. Update interface func added (need to fix).

---
+++ AnotherWeather/CurrentDateAndBG.swift
@@ -0,0 +1,50 @@
+//
+//  CurrentData.swift
+//  AnotherWeather
+//
+//  Created by Никита Зюзин on 11.12.2021.
+//
+
+import Foundation
+import UIKit
+
+class CurrentDateAndBG {
+
+var Currentdata: String!
+private let dateFormatter = DateFormatter()
+var currentBackGround : UIImage!
+
+func formatData() {
+dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
+dateFormatter.dateStyle = .long
+dateFormatter.locale = Locale(identifier: "ru_RU")
+let dateInFormat = dateFormatter.string(from: Date())
+Currentdata = dateInFormat
+}
+
+func switchBG() {
+    
+    let currentMonth = Calendar.current.component(.month, from: Date())
+    var BGarray = [""]
+    
+    switch currentMonth {
+    case 1:  BGarray = ["winter_01", "winter_02", "winter_03"]
+    case 2:  BGarray = ["winter_04", "winter_05", "winter_06"]
+    case 3:  BGarray = ["spring_01", "spring_02", "spring_03"]
+    case 4:  BGarray = ["spring_04", "spring_05", "spring_06"]
+    case 5:  BGarray = ["spring_07", "spring_08", "spring_09"]
+    case 6:  BGarray = ["summer_01", "summer_02", "summer_03"]
+    case 7:  BGarray = ["summer_04", "summer_05", "summer_06"]
+    case 8:  BGarray = ["summer_07", "summer_08", "summer_09"]
+    case 9:  BGarray = ["autumn_01", "autumn_02", "autumn_03"]
+    case 10:  BGarray = ["autumn_04", "autumn_05", "autumn_06"]
+    case 11:  BGarray = ["autumn_07", "autumn_08", "autumn_09"]
+    case 12:  BGarray = ["winter_07", "winter_08", "winter_09"]
+            
+    default: return
+        }
+    let backGroundName = BGarray.randomElement()
+    currentBackGround = UIImage(named: backGroundName!)
+    }
+
+}
--- AnotherWeather/ViewController.swift
+++ AnotherWeather/ViewController.swift
@@ -9,15 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentBackground: UIImageView!
     @IBOutlet weak var currentData: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelTempLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var feelText: UILabel!
+    @IBOutlet weak var searchButtonOut: UIButton!
+    
+    @IBAction func searchButtonAct(_ sender: UIButton) {
+        
+    }
+    
+    
+    private var currentDataBG = CurrentDateAndBG()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+        interfaceUpdate()
+        currentDataBG.switchBG()
+        currentDataBG.formatData()
+        currentData.text = String(describing: currentDataBG.Currentdata!)
+        currentBackground.image = currentDataBG.currentBackGround
         
-        formatData()
-        currentData.text = String(describing: Currentdata!)
     }
 
+    func interfaceUpdate() {
+        
+        
+        
+//        let logo = UIImage(named: "солнце")
+//        let imageView = UIImageView(image: logo)
+//        imageView.image = imageView.image!.withRenderingMode(.alwaysTemplate)
+//        imageView.tintColor = UIColor.white
+//        self.weatherIcon = imageView
+//
+//        weatherIcon.image = imageView.image!.withRenderingMode(.alwaysTemplate)
+//        imageView.tintColor = UIColor.white
+//        self.weatherIcon = imageView
+        
+        let randomColor = UIColor.init(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1)
+        
+        weatherIcon.tintColor = randomColor
+        temperatureLabel.textColor = randomColor
+        feelTempLabel.textColor = randomColor
+        cityNameLabel.textColor = randomColor
+        currentData.textColor = randomColor
+        feelText.textColor = randomColor
+        searchButtonOut.backgroundColor = randomColor
+    }
+    
 
 }
 
